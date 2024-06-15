eb52b365a4216f613c0cb776c5e5d4aa20f7c887
Devollox <122895078+Devollox@users.noreply.github.com>
2024-06-15 22:42:20 +0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/devollox",
 		label: "Twitter",
 		handle: "@chronark_",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:devollox@.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "devollox@.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/devollox",
 		label: "Github",
-		handle: "chronark",
+		handle: "devollox",
 	},
 ];
 
